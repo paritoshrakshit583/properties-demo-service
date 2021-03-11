573cf2924a6c3246276c15663a74bd41943a18f8
paritoshrakshit583 <paritoshrakshit583@gmail.com>
2021-03-11 22:07:55 +0530

    For remote

---
+++ src/main/java/com/demo/microservices/demopropertyaccessservice/PropertiesDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.microservices.demopropertyaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesDemoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/microservices/demopropertyaccessservice/PropertiesDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.microservices.demopropertyaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
